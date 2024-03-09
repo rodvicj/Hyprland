dd598f75316ff80dce827b2405e92d3283e8868a
rodvicj <rodvicj@gmail.com>
2024-03-09 22:36:02 +0800

    workaround for flameshot

--- src/protocols/Screencopy.cpp
+++ src/protocols/Screencopy.cpp
@@ -221,17 +221,6 @@ void CScreencopyProtocolManager::captureOutput(wl_client* client, wl_resource* r
 
     g_pHyprRenderer->makeEGLCurrent();
 
-    if (g_pHyprOpenGL->m_mMonitorRenderResources.contains(PFRAME->pMonitor)) {
-        const auto& RDATA = g_pHyprOpenGL->m_mMonitorRenderResources.at(PFRAME->pMonitor);
-        // bind the fb for its format. Suppress gl errors.
-#ifndef GLES2
-        glBindFramebuffer(GL_READ_FRAMEBUFFER, RDATA.offloadFB.m_iFb);
-#else
-        glBindFramebuffer(GL_FRAMEBUFFER, RDATA.offloadFB.m_iFb);
-#endif
-    } else
-        Debug::log(ERR, "No RDATA in screencopy???");
-
     PFRAME->shmFormat = g_pHyprOpenGL->getPreferredReadFormat(PFRAME->pMonitor);
     if (PFRAME->shmFormat == DRM_FORMAT_INVALID) {
         Debug::log(ERR, "No format supported by renderer in capture output");
